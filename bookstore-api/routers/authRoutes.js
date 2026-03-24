db07087bbc99e27fdbd4d8cb19faac6ee0beabfb
FIZZA FATIMA <fizza.abbaspk@gmail.com>
2026-03-25 04:28:42 +0500

    Cleanup: Removed mixed bookstore files and node_modules

--- bookstore-api/routers/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// Signup Route
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
-
-module.exports = router;
